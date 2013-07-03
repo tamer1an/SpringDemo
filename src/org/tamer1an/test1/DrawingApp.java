df541b976b1d330958aa07e3383c17d0f54e9ada
Tamerlan-i7 <trybynenko@gmail.com>
2013-07-04 02:42:39 +0300

    BeanFactory  setup

--- src/org/tamer1an/test1/DrawingApp.java
+++ src/org/tamer1an/test1/DrawingApp.java
@@ -1,9 +1,16 @@
 package org.tamer1an.test1;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import com.sun.xml.internal.bind.v2.util.XmlFactory;
+
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle();
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle) factory.getBean("triangle");
+		
 		triangle.draw();
 	}
 }
