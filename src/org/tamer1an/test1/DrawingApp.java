8b419e7f1323cdcb8b5301ba711cac7e6f2f8d7d
Tamerlan-i7 <trybynenko@gmail.com>
2013-07-04 02:21:01 +0300

    firstAPP

---
+++ src/org/tamer1an/test1/DrawingApp.java
@@ -0,0 +1,9 @@
+package org.tamer1an.test1;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		Triangle triangle = new Triangle();
+		triangle.draw();
+	}
+}
---
+++ src/org/tamer1an/test1/Triangle.java
@@ -0,0 +1,8 @@
+package org.tamer1an.test1;
+
+public class Triangle {
+
+	public void draw() {
+		System.out.println("triangle has been drawn");
+	}
+}
