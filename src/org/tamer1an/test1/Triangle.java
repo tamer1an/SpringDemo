b6a8321634e3ba6857acf23df0398c589eedac38
Tamerlan-i7 <trybynenko@gmail.com>
2013-07-05 02:46:15 +0300

    object injected from xml

--- src/org/tamer1an/test1/Triangle.java
+++ src/org/tamer1an/test1/Triangle.java
@@ -1,35 +1,36 @@
 package org.tamer1an.test1;
 
 public class Triangle {
+	private Point PointA;
+	private Point PointB;
+	private Point PointC;
 	
 	
-	private String type;
-	private int height;
-	
-	public Triangle(String type){
-		this.type = type;
-	}
-	
-	public Triangle(int height){
-		this.height = height;
-	}
-	
-	public Triangle(String type, int height){
-		this.type = type;
-		this.height = height;
-	}
-	
-	public String getType() {
-		return type;
-	}
-	
-	public int getHeight() {
-		return height;
+	public Point getPointA() {
+		return PointA;
 	}
 
+	public void setPointA(Point pointA) {
+		PointA = pointA;
+	}
 
+	public Point getPointB() {
+		return PointB;
+	}
+
+	public void setPointB(Point pointB) {
+		PointB = pointB;
+	}
+
+	public Point getPointC() {
+		return PointC;
+	}
+
+	public void setPointC(Point pointC) {
+		PointC = pointC;
+	}
 
 	public void draw() {
-		System.out.println(getType()+ " triangle has been drawn height="+ getHeight());
+		System.out.println(getPointA().getX() + " - "+getPointB().getY());
 	}
 }
