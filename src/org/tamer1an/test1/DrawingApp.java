d36d4deaa90a58dab4ec2d8d0bec843bc1694349
Tamerlan-i7 <trybynenko@gmail.com>
2013-07-05 01:41:40 +0300

    ApplicationContext 		Triangle triangle = (Triangle) context.getBean("triang
    		triangle.draw();
    
    ApplicationContext context = new
    ClassPathXmlApplicationContext("spring.xml");
    Triangle triangle = (Triangle) context.getBean("triangle");

--- src/org/tamer1an/test1/DrawingApp.java
+++ src/org/tamer1an/test1/DrawingApp.java
@@ -1,15 +1,14 @@
 package org.tamer1an.test1;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
-import com.sun.xml.internal.bind.v2.util.XmlFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
-	public static void main(String[] args) {
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle = (Triangle) factory.getBean("triangle");
+	public static void main(String[] args) {  //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		
 		triangle.draw();
 	}
