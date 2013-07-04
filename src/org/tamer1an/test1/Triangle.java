4d92c5dbae3efcd74eb94b378deebc27ad79eddd
Tamerlan-i7 <trybynenko@gmail.com>
2013-07-05 02:02:33 +0300

    property added - injected from xml

--- src/org/tamer1an/test1/Triangle.java
+++ src/org/tamer1an/test1/Triangle.java
@@ -1,8 +1,19 @@
 package org.tamer1an.test1;
 
 public class Triangle {
+	
+	
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 
 	public void draw() {
-		System.out.println("triangle has been drawn");
+		System.out.println(getType()+ " triangle has been drawn");
 	}
 }
