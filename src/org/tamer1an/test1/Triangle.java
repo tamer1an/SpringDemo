0b6615f47800c8d7750799420f7f5ee0e2e563a8
Tamerlan-i7 <trybynenko@gmail.com>
2013-07-05 02:12:26 +0300

    2 params in constructor
    2 params in constructor

--- src/org/tamer1an/test1/Triangle.java
+++ src/org/tamer1an/test1/Triangle.java
@@ -4,16 +4,28 @@ public class Triangle {
 	
 	
 	private String type;
+	private int height;
+	
+	public Triangle(String type){
+		this.type = type;
+	}
+	
+	public Triangle(String type, int height){
+		this.type = type;
+		this.height = height;
+	}
 	
 	public String getType() {
 		return type;
 	}
-
-	public void setType(String type) {
-		this.type = type;
+	
+	public int getHeight() {
+		return height;
 	}
 
+
+
 	public void draw() {
-		System.out.println(getType()+ " triangle has been drawn");
+		System.out.println(getType()+ " triangle has been drawn height="+ getHeight());
 	}
 }
