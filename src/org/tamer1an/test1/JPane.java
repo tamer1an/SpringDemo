ac2aca61d171267a0f5fc7abfcab22c425ea30cc
Tamerlan-i7 <trybynenko@gmail.com>
2013-07-08 23:19:57 +0300

    jpane&scanner

---
+++ src/org/tamer1an/test1/JPane.java
@@ -0,0 +1,14 @@
+package org.tamer1an.test1;
+import javax.swing.*;
+
+public class JPane {
+	public static void main(String[] args)
+	{
+		System.out.println("It is program My4");
+		String name=JOptionPane.showInputDialog("What is  your name?");
+		
+		System.out.println("Answer was: "+ name);
+		
+		System.exit(0);
+	}
+}
---
+++ src/org/tamer1an/test1/testScanner.java
@@ -0,0 +1,16 @@
+package org.tamer1an.test1;
+import java.util.Scanner;
+
+
+public class testScanner {
+
+	public static void main(String[] args) {
+		  Scanner listen = new Scanner(System.in);
+		  System.out.print(listen.nextLine());
+		  
+		  listen.close();
+	}
+
+}
+
+
